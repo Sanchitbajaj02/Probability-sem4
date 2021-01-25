11a052a7a8b22f6d3a6064008a627919d4a9b155
Sanchit Bajaj <sanchit02@outlook.com>
2021-01-25 22:15:22 +0530

    lab03 complete update

--- Lab03/Lab03_ques04.m
+++ Lab03/Lab03_ques04.m
@@ -1,2 +1,6 @@
-n = input("Enter the number of students: ");
+// Lab03 ques03
 
+clc
+n = input("Enter the number of students: ");
+p = factorial(n) - factorial(n-1);
+printf("No of ways in which the head boy and head girl do not sit together: %.2f", p)
